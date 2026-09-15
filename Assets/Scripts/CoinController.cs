f2f0cc53b593844e9d012362e5fec9d51d8715e4
Michel Serejo Campos <michelserejocampos09@gmail.com>
2026-09-15 15:42:44 -0300

    finale

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,6 +4,7 @@ public class CoinController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
+            other.gameObject.SendMessage("ChangeTextCoin");
             Destroy(gameObject);
         }
     }
